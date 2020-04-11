50f20254ea2c378e4057ddd3aae694ac9ccab8f9
edmundo papi <edmundopierre@yahoo.com>
2020-04-11 13:29:51 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -13,9 +13,9 @@ class Artist
   def name=(name)
     @name= name
   end
+
   def name
     @name
-    #binding.pry
   end
 
   def self.all
@@ -23,61 +23,21 @@ class Artist
   end
 
   def add_song(song)
-       @songs << song
-       song.artist= self
-       song
-      #binding.pry
+    @songs << song
+    song.artist= self
+    song
   end
+
   def songs
     @songs
-  #  Song.all.select {|song| song.artist == self}
-    #binding.pry
   end
    def self.find_or_create_by_name(name)
      find_name =self.all.detect{|artist| artist.name == name}
-
      find_name ? find_name : self.new(name)
-
    end
 
    def print_songs
     songs.each{|song| puts song.name}
+
      end
-  #
-  #     end
-  #   @@all = []
-  #  def initialize(name)
-  #
-  #     @name = name
-  #     @@all << self
-  #  end
-  #
-  # def self.all
-  #    @@all
-  #   # binding.pry
-  # end
-  #
-  # def add_song(song)
-  #   song.artist = self
-  # end
-  #
-  #  def songs
-  #   Song.all.select {|song| song.artist == self}
-  #    #binding.pry
-  #  end
-  #
-  # def self.find_or_create_by_name(name)
-  #    #self.artist
-  #   artist_object =  self.all.find do |artist|
-  #
-  #        artist.name == name
-  #      end
-  #
-  #      artist_object ? artist_object : self.new(name)
-  #    end
-  #
-  #    def print_songs
-  #
-  #      songs.each{|song| song.name}
-  #    end
 end
