5c36d5fd4fc23af9cc73c9ddaa407d73b9693a5d
edmundo papi <edmundopierre@yahoo.com>
2020-04-11 12:34:04 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -36,7 +36,7 @@ class Artist
 
       end
   def self.find(name)
-         self.all.detect {|artist| artist.name == name}
+         return self.all.detect {|artist| artist.name == name}
   end
    def self.find_or_create_by_name(name)
      self.all.detect {|artist| artist.name == name} || Artist.new(name).save
