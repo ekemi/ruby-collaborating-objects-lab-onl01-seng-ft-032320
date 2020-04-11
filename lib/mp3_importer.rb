94d8af1dffb09643d730b0bdfead0e1506f70929
edmundo papi <edmundopierre@yahoo.com>
2020-04-11 14:25:07 +0000

    Automatically backed up by Learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -5,7 +5,7 @@ class MP3Importer
    end
 
    def files
-     Dir.entries(path).map{|files| files.split("-").last if files.length ==4}
+     Dir.entries(path).map{|files| files.split("-").last if files.length ==4 files}
    end
 
 
